4480c3fbe213ae580f410d80dbfd04eff10a4a86
Iuliana <sendmailtoiuliana@gmail.com>
2024-05-24 03:08:24 +0300

    add portfolio

---
+++ src/components/stocks/portfolio/Portfolio.vue
@@ -0,0 +1,26 @@
+<template>
+     <div>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"></Stock>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import Stock from './Stock.vue';
+
+    export default {
+        components: {
+            Stock
+        },
+
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        } 
+    }
+</script>
+
+<style scoped>
+
+</style>    
